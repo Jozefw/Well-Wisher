d593c4c1159e7b69501abc6857613d94df7fc2d0
Jozef <jozefw@sbcglobal.net>
2017-09-09 17:40:48 -0700

    add function constructor and prototype methods

--- wisher.js
+++ wisher.js
@@ -1,6 +1,18 @@
 (function(global,$){
-var wellWisher = function(first, last, lang){
 
+var wellWisher = function(first, last, lang){
+		return new wellWisher.init(first, last, lang);
 };
+
+		wellWisher.prototype = {};
+
+		wellWisher.init = function(first, last, lang){
+
+			var self = this;
+			self.first = first || '';
+			self.last = last || '';
+			self.lang = lang || 'english';
+		};
+
 
 })(window,jQuery);
